d8a821ebc42d183fe14b90b64b2566e9ccf45d06
seungilahn <dkstm95@naver.com>
2020-12-04 13:30:37 +0900

    Module 3 Week 3.

--- Module3/UCSDUnfoldingMaps/src/module3/EarthquakeCityMap.java
+++ Module3/UCSDUnfoldingMaps/src/module3/EarthquakeCityMap.java
@@ -77,7 +77,9 @@ public class EarthquakeCityMap extends PApplet {
 	    // to create a new SimplePointMarker for each PointFeature in 
 	    // earthquakes.  Then add each new SimplePointMarker to the 
 	    // List markers (so that it will be added to the map in the line below)
-	    
+	    for (PointFeature eq : earthquakes) {
+	    	markers.add(createMarker(eq));
+	    }
 	    
 	    // Add the markers to the map so that they are displayed
 	    map.addMarkers(markers);
@@ -108,6 +110,8 @@ public class EarthquakeCityMap extends PApplet {
 		// Here is an example of how to use Processing's color method to generate 
 	    // an int that represents the color yellow.  
 	    int yellow = color(255, 255, 0);
+	    int blue = color(0, 0, 255);
+	    int red = color(255, 0, 0);
 		
 		// TODO (Step 4): Add code below to style the marker's size and color 
 	    // according to the magnitude of the earthquake.  
@@ -116,7 +120,18 @@ public class EarthquakeCityMap extends PApplet {
 	    // Rather than comparing the magnitude to a number directly, compare 
 	    // the magnitude to these variables (and change their value in the code 
 	    // above if you want to change what you mean by "moderate" and "light")
-	    
+	    if (mag < THRESHOLD_LIGHT) {
+	    	marker.setColor(blue);
+	    	marker.setRadius(10);
+	    }
+	    else if (mag < THRESHOLD_MODERATE) {
+	    	marker.setColor(yellow);
+	    	marker.setRadius(20);
+	    }
+	    else {
+	    	marker.setColor(red);
+	    	marker.setRadius(30);
+	    }
 	    
 	    // Finally return the marker
 	    return marker;
@@ -134,6 +149,9 @@ public class EarthquakeCityMap extends PApplet {
 	private void addKey() 
 	{	
 		// Remember you can use Processing's graphics methods here
-	
+		rect(20, 50, 160, 250);
+		fill(200, 200, 200);
+		text("Earthquake Key", 40, 70);
+		fill(50);
 	}
 }
