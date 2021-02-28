8b405a69804c03d45fe67fdbc58beeceee8e8f2e
dkstm95 <dkstm95@naver.com>
2021-02-28 22:00:16 +0900

    Last Commit.

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -1,26 +1,12 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.EntityManager;
-import java.util.List;
-
-@Repository
-@RequiredArgsConstructor
+@Getter @Setter
 public class OrderSearch {
 
-    private final EntityManager em;
-
-    public void save(Order order) {
-        em.persist(order);
-    }
-
-    public Order findOne(Long id) {
-        return em.find(Order.class, id);
-    }
-
-    /*public List<Order> findAll(OrderSearch orderSearch) {
-    }*/
+    private String memberName;
+    private OrderStatus orderStatus;
 }
